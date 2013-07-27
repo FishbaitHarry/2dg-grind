cc78005a37f0d7a68320937ef9d7f698d5ac56b4
Simon Maj <fishbaitharry@gmail.com>
2013-07-27 02:39:39 +0200

    first jasmine test

--- spec/smoke.spec.js
+++ spec/smoke.spec.js
@@ -1,18 +1,34 @@
-var bm = require('../src/battle');
-var cm = require('../src/character');
+describe( "My coding environment", function() {
 
-var m1 = new cm.GenericMonster();
-var m2 = new cm.GenericMonster();
+	it( "can require my project's files", function() {
+		var bm = require('../src/battle');
+		var cm = require('../src/character');
 
-var exBattle = new bm.Battle({
-	participants: [m1, m2]
+		expect(bm).toBeDefined();
+		expect(cm).toBeDefined();
+	});
+
+	it( "can run the simplest battle sequence", function() {
+		var bm = require('../src/battle');
+		var cm = require('../src/character');
+
+		var m1 = new cm.GenericMonster();
+		var m2 = new cm.GenericMonster();
+
+		var exBattle = new bm.Battle({
+			participants: [m1, m2]
+		});
+
+		expect(exBattle.round).toEqual(1);
+
+		exBattle.resolveCombatRound();
+		exBattle.resolveCombatRound();
+		exBattle.resolveCombatRound();
+		exBattle.resolveCombatRound();
+
+		expect(exBattle.round).toEqual(5);
+
+		// bm.BattleLogFormatter.consumeLog(exBattle.log);
+	});
+
 });
-exBattle.resolveCombatRound();
-exBattle.resolveCombatRound();
-exBattle.resolveCombatRound();
-exBattle.resolveCombatRound();
-
-bm.BattleLogFormatter.consumeLog(exBattle.log);
-//console.log(JSON.stringify(exBattle));
-//console.log(JSON.stringify(exBattle.log));
-console.log("trololo");
